6a13e2b26b6a64df32174b64ade6141af640ab8a
Louisevirtudazo <louise.virtudazo@stylebyglobal.com>
2024-01-28 23:41:38 +0800

    env configs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
+
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migrations', function () {
+    Artisan::call('optimize:clear');
+    Artisan::call('migrate:fresh --seed');
+
+    return 'migrations ran';
 });
